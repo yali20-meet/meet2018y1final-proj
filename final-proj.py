4cc92ffbc7f1f648311c7d4b17eef36d19dcbe9b
yali20-meet <yali20@meet.mit.edu>
2018-08-15 02:47:58 -0400

    Stopping here

--- final-proj.py
+++ final-proj.py
@@ -35,6 +35,14 @@ sea_turtle_pos.append(sea_turtle.pos())
 print(sea_turtle_pos)
 turtle.register_shape("sea-turtle.gif")
 sea_turtle.shape("sea-turtle.gif")
+
+#getting the x and y of the turtles
+x_p = player.xcor()
+y_p = player.ycor()
+x_b = bags.xcor()
+y_b = bags.ycor()
+x_t = sea_turtle.xcor()
+y_t = sea_turtle.ycor()
  
 
 max_x = 683
@@ -50,12 +58,15 @@ while y_pos > -394: #as long as the bags dont touch the border make them fall
     bags.speed(1)
     y_pos = y_pos - 20
     bags.goto(rand , y_pos)
-    print(bags.pos())
-    print(sea_turtle_pos)
-    if bags.pos() == sea_turtle.pos():   #make the turtle disapear if the bag
+    #print(bags.pos())
+    #print(sea_turtle_pos)
+    if (abs(x_b - x_t) < 20) and (abs(y_b - y_t)< 20):   #make the turtle disapear if the bag
         sea_turtle.ht()                                   #touches it
-
-
+        
+        print(bags.pos())
+        print(sea_turtle.pos())
+        print(abs(y_b - y_t))
+        
     def move_left(): #move left function.
         x = player.xcor()
         x = x - 20
@@ -75,10 +86,12 @@ while y_pos > -394: #as long as the bags dont touch the border make them fall
     turtle.onkey(move_left, 'Left')   #make the turtle move left.
     turtle.onkey(move_right, 'Right') #make the turtle move right.
 
-    if bags.pos() == player.pos():
+
+    if abs(x_p - x_b) < 20 and abs(y_p - y_b) < 20: #makes the bag disapear if they touch the net
         bags.ht()
 
 bags.ht()
+
 
         
 
